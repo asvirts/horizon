04654c3a50de3a21f59b9833d614c218154d52c8
Andrew Virts <asvirts@outlook.com>
2025-01-06 11:47:33 -0600

    Add new columns to tasks table migration: title, due_date, status, priority, category, assignee, reporter, project, and is_completed

--- database/migrations/2025_01_06_015449_create_tasks_table.php
+++ database/migrations/2025_01_06_015449_create_tasks_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->date('due_date');
+            $table->string('status');
+            $table->string('priority');
+            $table->string('category');
+            $table->string('assignee');
+            $table->string('reporter');
+            $table->string('project');
+            $table->boolean('is_completed')->default(false);
         });
     }
 
