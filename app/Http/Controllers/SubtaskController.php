3505be70a42d43fbeeab37dae07a606731590552
Andrew Virts <asvirts@outlook.com>
2025-01-05 19:57:24 -0600

    adding new models

---
+++ app/Http/Controllers/SubtaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subtask;
+use App\Http\Requests\StoreSubtaskRequest;
+use App\Http\Requests\UpdateSubtaskRequest;
+
+class SubtaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubtaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Subtask $subtask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Subtask $subtask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubtaskRequest $request, Subtask $subtask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Subtask $subtask)
+    {
+        //
+    }
+}
