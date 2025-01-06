d1d65aeafbbb08ce122ec0b9e8aeed160ef77104
Andrew Virts <asvirts@outlook.com>
2025-01-06 08:03:22 -0600

    Add relationships to Comment model for Task, Subtask, and User

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,4 +9,19 @@ class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function subtask()
+    {
+        return $this->belongsTo(Subtask::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
